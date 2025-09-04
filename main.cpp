4ac33e463cad64a836693bd2b804091f63ade71a
alecedur <alecu0825@gmail.com>
2025-09-03 23:37:02 -0600

    added collision management for the game

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
+#include <raymath.h>
 
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
@@ -7,16 +9,62 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+double lastUpdateTime = 0.0;
+
+bool elementInDeque(Vector2 element, std::deque<Vector2> deque){
+    for(unsigned int i = 0; i < deque.size(); i++){
+        if(Vector2Equals(deque[i], element)){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool eventTriggered(double interval){
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval){
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+class Snake {
+public:
+    std::deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+    Vector2 direction = {1, 0};
+    bool addSegment = false;
+
+    void Draw(){
+        for(unsigned int i = 0; i < body.size(); i++){
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment =  Rectangle{x * cellSize, y * cellSize, (float)cellSize, (float)cellSize};
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
+        }
+    }
+
+    void Update(){
+        body.push_front(Vector2Add(body[0], direction));
+        if (addSegment == true){
+            addSegment = false;
+        }
+        else{
+            body.pop_back();
+        }
+        
+    }
+};
 class Food {
 public:
     Vector2 position;
     Texture2D texture;
 
-    Food(){
+    Food(std::deque<Vector2> snakeBody){
         Image image = LoadImage("Graphics/food.png");
         texture = LoadTextureFromImage(image);
         UnloadImage(image);
-        position = generateRandomPos();
+        position = generateRandomPos(snakeBody);
     }
 
     ~Food(){
@@ -28,10 +76,55 @@ public:
 
     }
 
-    Vector2 generateRandomPos(){
+    Vector2 generateRandomCell(){
         float x = GetRandomValue(0, cellCount - 1);
         float y = GetRandomValue(0, cellCount - 1);
         return Vector2{x, y};
+    }
+
+    Vector2 generateRandomPos(std::deque<Vector2> snakeBody){
+        
+        Vector2 position = generateRandomCell();
+        while(elementInDeque(position, snakeBody)){
+            position = generateRandomCell();
+        }
+        return position;
+    }
+};
+
+class Game {
+public:
+    Snake snake = Snake();
+    Food food = Food(snake.body);
+
+    void Draw(){
+        food.Draw();
+        snake.Draw();
+    }
+
+    void Update(){
+        snake.Update();
+        checkCollisionWithFood();
+    }
+
+    void checkCollisionWithFood(){
+        if(Vector2Equals(snake.body[0], food.position)){
+            food.position = food.generateRandomPos(snake.body);
+            snake.addSegment = true;
+        }
+    }
+
+    void checkCollisionWithEdges(){
+        if(snake.body[0].x == cellCount || snake.body[0].x == -1){
+            gameOver();
+        }
+        else if (snake.body[0].y == cellCount || snake.body[0].y == -1){
+            gameOver();
+        }
+    }
+    
+    void gameOver(){
+        std::cout << "Game Over" << std::endl;
     }
 };
 
@@ -41,13 +134,34 @@ int main () {
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
     SetTargetFPS(60);
 
-    Food food = Food();
+    Game game = Game();
 
     while(WindowShouldClose() == false){
         BeginDrawing();
+        
+        if (eventTriggered(0.2)){
+            game.Update();
+        }
+
+        if(IsKeyPressed(KEY_UP) && game.snake.direction.y != 1){
+            game.snake.direction = {0, -1};
+        }
+
+        if(IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1){
+            game.snake.direction = {0, 1};
+        }
+
+        if(IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1){
+            game.snake.direction = {-1, 0};
+        }
+
+        if(IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1 ){
+            game.snake.direction = {1, 0};
+        }
+
         // Drawing
         ClearBackground(green);
-        food.Draw();
+        game.Draw();
 
         EndDrawing();
     }
